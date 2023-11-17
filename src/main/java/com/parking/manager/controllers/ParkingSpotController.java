b251eb281a800e8c7ec97693d29606cd1839b507
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-17 16:53:18 -0300

    [:sparkles:] | create: controller - ParkingSpot > save (api)

--- src/main/java/com/parking/manager/controllers/ParkingSpotController.java
+++ src/main/java/com/parking/manager/controllers/ParkingSpotController.java
@@ -1,10 +1,21 @@
 package com.parking.manager.controllers;
 
+import com.parking.manager.dto.ParkingSpotDTO;
+import com.parking.manager.models.ParkingSpotModel;
 import com.parking.manager.services.ParkingSpotService;
+import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
@@ -12,4 +23,23 @@ import org.springframework.web.bind.annotation.RestController;
 public class ParkingSpotController {
     @Autowired
     private ParkingSpotService service;
+
+    @PostMapping
+    public ResponseEntity<Object> save(
+            @RequestBody
+            @Valid
+            ParkingSpotDTO DTO
+    ) {
+
+        if(this.service.alreadyExists(DTO)) return ResponseEntity.status(HttpStatus.CONFLICT).body("Conflict: Data Already Exists");
+
+        var spot = new ParkingSpotModel();
+        BeanUtils.copyProperties(DTO, spot);
+        spot.setRegistrationDate(LocalDateTime.now(ZoneId.of("UTC")));
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.save(spot))
+    }
+
+
+
 }
