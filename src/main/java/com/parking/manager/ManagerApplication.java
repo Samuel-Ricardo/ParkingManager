62bb2989dd9516da47463209c48ae246d1e8be9c
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-17 15:47:09 -0300

    [:sparkles:] | create: route - GET > index (api)

--- src/main/java/com/parking/manager/ManagerApplication.java
+++ src/main/java/com/parking/manager/ManagerApplication.java
@@ -2,12 +2,19 @@ package com.parking.manager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ManagerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ManagerApplication.class, args);
 	}
 
+	@GetMapping("/")
+	public String index() {
+		return "{message: \"Hello World!\"}";
+	}
 }
