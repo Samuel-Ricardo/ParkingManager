6243c2c091d2400110b3675790930f6b7fe5312e
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-17 16:11:16 -0300

    [:sparkles:] | create: model - parking spot (api)

---
+++ src/main/java/com/parking/manager/models/ParkingSpotModel.java
@@ -0,0 +1,35 @@
+package com.parking.manager.models;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "TB_PARKING_SPOT")
+public class ParkingSpotModel implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+    @Column(nullable = false, unique = true, length = 10)
+    private String parkingSpotNumber;
+    @Column(nullable = false, unique = true, length = 7)
+    private String licensePlate;
+    @Column(nullable = false, length = 70)
+    private String brand;
+    @Column(nullable = false, length = 70)
+    private String model;
+    @Column(nullable = false, length = 70)
+    private String color;
+    @Column(nullable = false)
+    private LocalDateTime registrationDate;
+    @Column(nullable = false, length = 130)
+    private String responsibleName;
+    @Column(nullable = false, length = 30)
+    private String apartment;
+    @Column(nullable = false, length = 30)
+    private String block;
+}
