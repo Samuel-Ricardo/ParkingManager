786918aead0f4e67aa0823ea841c7f96ed235333
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-18 01:26:26 -0300

    [:passport_control:] | implements: UserDetails - model > user (auth)

--- src/main/java/com/parking/manager/models/UserModel.java
+++ src/main/java/com/parking/manager/models/UserModel.java
@@ -1,9 +1,11 @@
 package com.parking.manager.models;
 
 import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.io.Serializable;
+import java.util.Collection;
 import java.util.List;
 import java.util.UUID;
 
@@ -27,5 +29,39 @@ public class UserModel implements UserDetails, Serializable {
             inverseJoinColumns = @JoinColumn(name = "role_id")
     )
     private List<RoleModel> roles;
-    
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.roles;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
