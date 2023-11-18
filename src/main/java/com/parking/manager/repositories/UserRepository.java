e02e1d1d5eede8e2e7014039df8c0671ace5089e
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-18 01:32:51 -0300

    [:card_file_box:] | create: repository - user (jpa)

---
+++ src/main/java/com/parking/manager/repositories/UserRepository.java
@@ -0,0 +1,14 @@
+package com.parking.manager.repositories;
+
+import com.parking.manager.models.UserModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserModel, UUID> {
+
+    Optional<UserModel> findByUserName(String username);
+}
