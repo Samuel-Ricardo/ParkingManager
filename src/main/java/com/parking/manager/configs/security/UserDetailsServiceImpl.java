5286f19e56df1929edbaf8cf353b69915133eb4b
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-18 01:40:58 -0300

    [:passport_control:] | implements: UserDetails - service (config/security)

--- src/main/java/com/parking/manager/configs/security/UserDetailsServiceImpl.java
+++ src/main/java/com/parking/manager/configs/security/UserDetailsServiceImpl.java
@@ -1,9 +1,12 @@
 package com.parking.manager.configs.security;
 
+import com.parking.manager.models.UserModel;
 import com.parking.manager.repositories.UserRepository;
 import jakarta.transaction.Transactional;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -17,4 +20,20 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     }
 
 
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        var user = repository
+                .findByUserName(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
+
+        return new User(
+                user.getUsername(),
+                user.getPassword(),
+                true,
+                true,
+                true,
+                true,
+                user.getAuthorities()
+        );
+    }
 }
