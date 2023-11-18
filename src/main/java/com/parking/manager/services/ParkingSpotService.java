d9e0963eff44d0b58cbb7b24c4b4f2e21d01abed
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-18 13:30:11 -0300

    [:sparkles:] | create: service - ParkingSpot > interface (main)

--- src/main/java/com/parking/manager/services/ParkingSpotService.java
+++ src/main/java/com/parking/manager/services/ParkingSpotService.java
@@ -1,58 +1,20 @@
 package com.parking.manager.services;
 
-import com.parking.manager.dto.ParkingSpotDTO;
 import com.parking.manager.models.ParkingSpotModel;
-import com.parking.manager.repositories.ParkingSpotRepository;
-import jakarta.transaction.Transactional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 import java.util.UUID;
 
-@Service
-public class ParkingSpotService {
+public interface ParkingSpotService {
 
-    private final ParkingSpotRepository repository;
-
-    public ParkingSpotService(ParkingSpotRepository repository) {
-        this.repository = repository;
-    }
-
-    @Transactional
-    public ParkingSpotModel save(ParkingSpotModel spot) {
-        return repository.save(spot);
-    }
-
-     public boolean existsByLicensePlate(String licensePlate) {
-         return repository.existsByLicensePlate(licensePlate);
-     }
-
-     public boolean existsByParkingSpotNumber(String parkingSpotNumber) {
-        return repository.existsByParkingSpotNumber(parkingSpotNumber);
-     }
-
-     public boolean existsByApartmentAndBlock(String apartment, String block) {
-        return repository.existsByApartmentAndBlock(apartment, block);
-     }
-
-     @Transactional
-     public boolean alreadyExists (ParkingSpotDTO DTO) {
-        return this.existsByLicensePlate(DTO.licensePlate()) && this.existsByParkingSpotNumber(DTO.parkingSpotNumber()) && this.existsByApartmentAndBlock(DTO.apartment(), DTO.block());
-     }
-
-     public Page<ParkingSpotModel> findAll(Pageable pageable) {
-        return repository.findAll(pageable);
-     }
-
-     public Optional<ParkingSpotModel> findById(UUID id) {
-        return repository.findById(id);
-     }
-
-     @Transactional
-     public void delete(ParkingSpotModel spot) {
-        repository.delete(spot);
-     }
+    ParkingSpotModel save(ParkingSpotModel parkingSpotModel);
+    boolean existsByLicensePlate(String licensePlateCar);
+    boolean existsByParkingSpotNumber(String parkingSpotNumber);
+    boolean existsByApartmentAndBlock(String apartment, String block);
+    Page<ParkingSpotModel> findAll(Pageable pageable);
+    Optional<ParkingSpotModel> findById(UUID id);
+    void delete(ParkingSpotModel parkingSpotModel);
 
 }
